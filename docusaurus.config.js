f74d10ff1ea9663c1764fd71599573c3b9f870a9
nsorensen2112 <51712183+nsorensen2112@users.noreply.github.com>
2025-06-27 17:52:43 -0600

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,59 +64,31 @@ projectName: 'my-docs-site',  // Your GitHub repo name
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://github.com/KneekoCo/my-docs-site',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-             
-              {
-                label: 'GitHub',
-                href: 'https://github.com/KneekoCo/my-docs-site'
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} KneekoCo, LLC. Built with Docusaurus.`,
+  themeConfig: {
+  image: 'img/docusaurus-social-card.jpg',
+
+  navbar: {
+    title: 'KneekoCo',
+    logo: {
+      alt: 'My Site Logo',
+      src: 'img/logo.svg',
+    },
+    items: [
+      {
+        href: 'https://github.com/KneekoCo/my-docs-site',
+        label: 'GitHub',
+        position: 'right',
       },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
-};
+    ],
+  },
+  footer: {
+    // keep your footer as-is
+  },
+  prism: {
+    theme: prismThemes.github,
+    darkTheme: prismThemes.dracula,
+  },
+},
+}
 
 export default config;
