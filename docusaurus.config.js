0eccccac0f2276da017f66a2481639bc027ccd90
KneekoCo <Nsorensen@KneekoCo.com>
2025-06-27 18:53:26 -0600

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,5 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
-import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -14,103 +7,85 @@ const config = {
   tagline: 'Lets Document Easy',
   favicon: 'img/ThaLogo.png',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
+  url: 'https://kneekoco.github.io',
+  baseUrl: '/my-docs-site/',
 
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-url: 'https://kneekoco.github.io',
-baseUrl: '/my-docs-site/',
-
-organizationName: 'KneekoCo',
-projectName: 'my-docs-site',
-
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
- // Your GitHub username
-  // Your GitHub repo name
+  organizationName: 'KneekoCo',
+  projectName: 'my-docs-site',
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
 
-
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/KneekoCo/my-docs-site',
+          editUrl: 'https://github.com/KneekoCo/my-docs-site',
         },
         blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
-      }),
+      },
     ],
   ],
 
   themeConfig: {
-  image: 'img/ThaLogo.png',
-
-  navbar: {
-    title: 'KneekoCo',
-    logo: {
-      alt: 'My Site Logo',
-      src: '/static/img/logo.png',
-    },
-    items: [
-      {
-        href: 'https://github.com/KneekoCo/my-docs-site',
-        label: 'GitHub',
-        position: 'right',
+    image: 'img/ThaLogo.png',
+    navbar: {
+      title: 'KneekoCo',
+      logo: {
+        alt: 'KneekoCo Logo',
+        src: 'img/ThaLogo.png', // ✅ No leading slash, must match file in /static/img/
       },
-    ],
-  },
- footer: {
-  style: 'dark',
-  links: [
-    {
-      title: 'Docs',
       items: [
         {
-          label: 'Home',
-         to: 'docs/intro',
-
-        },
-      ],
-    },
-    {
-      title: 'Community',
-      items: [
-        {
-          label: 'GitHub',
           href: 'https://github.com/KneekoCo/my-docs-site',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
-  ],
-  copyright: `Copyright © ${new Date().getFullYear()} KneekoCo, LLC. Built with Docusaurus.`,
-}
-
-},
-}
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Home',
+              to: 'docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/KneekoCo/my-docs-site',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} KneekoCo, LLC. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
+};
 
 export default config;
