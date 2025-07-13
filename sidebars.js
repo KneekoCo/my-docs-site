9ef3585726302dd61bb4b0a16ee11f811d205136
KneekoCo <Nsorensen@KneekoCo.com>
2025-07-13 10:50:22 -0600

    5

--- sidebars.js
+++ sidebars.js
@@ -3,8 +3,8 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   tutorialSidebar: [
-    'welcome.mdx',               // 👈 This matches /docs/welcome.mdx
-    'notion-task-tracker.mdx',   // 👈 This matches /docs/notion-task-tracker.mdx
+    'welcome',               // 👈 This matches /docs/welcome.mdx
+    'notion-task-tracker',   // 👈 This matches /docs/notion-task-tracker.mdx
   ],
 };
 
